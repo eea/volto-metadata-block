86bcc59ca2a804fbf471e3bca8c4cdf76aee30a4
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-08-02 10:41:36 +0300

    Create the FieldEditor component

--- src/components/manage/Blocks/MetadataSection/EditMetadataSection.jsx
+++ src/components/manage/Blocks/MetadataSection/EditMetadataSection.jsx
@@ -5,6 +5,44 @@ import { BlockDataForm, Field, SidebarPortal } from '@plone/volto/components';
 import MetadataSectionSchema from './schema';
 import '@eeacms/volto-metadata-block/less/editor.less';
 
+function FieldEditor({
+  block,
+  onChangeField,
+  title,
+  schema,
+  fieldData,
+  fields,
+}) {
+  return (
+    <fieldset>
+      <legend aria-hidden="true">{title || 'Metadata section'}</legend>
+      {fields?.length
+        ? fields.map((value) => {
+            const { id: metadata_id } = value?.field || {};
+            if (!metadata_id) return '';
+            const field = schema.properties
+              ? schema.properties[metadata_id]
+              : null;
+            const required = schema?.required?.includes(metadata_id);
+            return (
+              <Field
+                {...field}
+                id={metadata_id}
+                value={fieldData[metadata_id]}
+                required={required}
+                onChange={(id, value) => {
+                  onChangeField(id, value);
+                }}
+                key={metadata_id}
+                block={block}
+              />
+            );
+          })
+        : 'No fields selected'}
+    </fieldset>
+  );
+}
+
 export default function EditMetadataSectionBlock(props) {
   const {
     selected,
@@ -16,8 +54,7 @@ export default function EditMetadataSectionBlock(props) {
     onChangeField,
   } = props;
   const schema = useSelector((state) => state?.schema?.schema || {});
-  let metadata_element = {};
-  metadata_element = metadata ? { ...metadata } : { ...properties };
+  let fieldData = metadata || properties;
 
   return (
     <div className={cx('block metadata-section', { selected: selected })}>
@@ -36,33 +73,14 @@ export default function EditMetadataSectionBlock(props) {
           />
         )}
       </SidebarPortal>
-
-      <fieldset>
-        <legend aria-hidden="true">{data.title || 'Metadata section'}</legend>
-        {data.fields?.length
-          ? data.fields.map((value) => {
-              const { id: metadata_id } = value?.field || {};
-              if (!metadata_id) return '';
-              const field = schema.properties
-                ? schema.properties[metadata_id]
-                : null;
-              const required = schema?.required?.includes(metadata_id);
-              return (
-                <Field
-                  {...field}
-                  id={metadata_id}
-                  value={metadata_element[metadata_id]}
-                  required={required}
-                  onChange={(id, value) => {
-                    onChangeField(id, value);
-                  }}
-                  key={metadata_id}
-                  block={block}
-                />
-              );
-            })
-          : 'No fields selected'}
-      </fieldset>
+      <FieldEditor
+        block={block}
+        onChangeField={onChangeField}
+        schema={schema}
+        fieldData={fieldData}
+        fields={data.fields || []}
+        title={data.title}
+      />
     </div>
   );
 }
