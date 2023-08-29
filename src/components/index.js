de3aa7ed26c84921ce11d09a568d5e318dd44baf
Alin Voinea <contact@avoinea.com>
2023-08-29 20:40:34 +0300

    test: Fix eslint and yarn i18n

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
-export MetadataBlockEdit from './manage/Blocks/Metadata/Edit';
-export MetadataBlockView from './manage/Blocks/Metadata/View';
+export { default as MetadataBlockEdit } from './manage/Blocks/Metadata/Edit';
+export { default as MetadataBlockView } from './manage/Blocks/Metadata/View';
 
-export MetadataSectionBlockEdit from './manage/Blocks/MetadataSection/EditMetadataSection';
-export MetadataSectionBlockView from './manage/Blocks/MetadataSection/ViewMetadataSection';
-export MetadataSectionLayoutSchema from './manage/Blocks/MetadataSection/LayoutSchema';
+export { default as MetadataSectionBlockEdit } from './manage/Blocks/MetadataSection/EditMetadataSection';
+export { default as MetadataSectionBlockView } from './manage/Blocks/MetadataSection/ViewMetadataSection';
+export { default as MetadataSectionLayoutSchema } from './manage/Blocks/MetadataSection/LayoutSchema';
 
 export {
   MetadataSectionListingView,
