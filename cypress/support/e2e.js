51f02b38960c8a45a13e08498e28c7b9a158fed5
dana-cfc4 <dana.comiselu4@gmail.com>
2024-03-28 14:27:24 +0200

    test: wait for content to load in cypress tests

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -22,6 +22,7 @@ import './commands';
 import '@cypress/code-coverage/support';
 
 export const slateBeforeEach = (contentType = 'Document') => {
+  cy.intercept('GET', `/**/*?expand*`).as('content');
   cy.autologin();
   cy.createContent({
     contentType: 'Document',
@@ -35,7 +36,7 @@ export const slateBeforeEach = (contentType = 'Document') => {
     path: 'cypress',
   });
   cy.visit('/cypress/my-page');
-  cy.waitForResourceToLoad('my-page');
+  cy.wait('@content');
   cy.navigate('/cypress/my-page/edit');
 };
 
