eb435736fc383419898284ce1b59f601aeca99df
Alin Voinea <contact@avoinea.com>
2026-03-25 17:54:03 +0200

    feat: Volto 18 support - refs #287700

--- cypress/e2e/02-dexterity-controlpanel-layout.cy.js
+++ cypress/e2e/02-dexterity-controlpanel-layout.cy.js
@@ -21,6 +21,7 @@ describe('ControlPanel: Dexterity Content-Types Layout', () => {
     // Wait a bit for draftjs to load, without this the title block
     // custom placeholder is missing and cypress gives a timeout error
     cy.wait(1000);
+    cy.contains('.tabs-wrapper .menu .item', 'Settings').click();
     cy.get('input[id="field-placeholder"]').type('Book title');
     cy.get('label[for="field-required"]').click();
     cy.get('label[for="field-fixed"]').click();
